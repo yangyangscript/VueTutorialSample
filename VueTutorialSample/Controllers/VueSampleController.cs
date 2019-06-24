d06ed1cb1713489cac0256f81db6fef479db24df
yangyangWork <1324954289@qq.com>
2019-06-24 17:35:13 +0800

    添加项目文件。

---
+++ VueTutorialSample/Controllers/VueSampleController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VueTutorialSample.Controllers
+{
+    public class VueSampleController : Controller
+    {
+        public ActionResult HellowVue()
+        {
+            return View();
+        }
+
+        //v-if 来确定元素是否显示
+        public ActionResult IfVue()
+        {
+            return View();
+        }
+
+        //v-for 循环
+        public ActionResult ForVue()
+        {
+            return View();
+        }
+
+        public ActionResult HandlerVue()
+        {
+            return View();
+        }
+    }
+}
