727b352670172990cb7835388bc96a29787c754f
yangyangWork <1324954289@qq.com>
2019-06-25 17:41:19 +0800

    到Input

--- VueTutorialSample/Controllers/VueSampleController.cs
+++ VueTutorialSample/Controllers/VueSampleController.cs
@@ -25,9 +25,25 @@ namespace VueTutorialSample.Controllers
             return View();
         }
 
+        //v-on:click @click
         public ActionResult HandlerVue()
         {
             return View();
         }
+
+        //v-model 双向绑定
+        public ActionResult IntPutMessage()
+        {
+            return View();
+        }
+
+        //1.prop传值 https://cn.vuejs.org/v2/guide/components-props.html
+        //2.v-bind 动态赋值
+        public ActionResult ComponentVue()
+        {
+            return View();
+        }
+
+
     }
 }
